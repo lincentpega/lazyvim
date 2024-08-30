592805b96568f72608f20943dd1698fb8d57b98f
lincentpega <imkrasnyukov@gmail.com>
2024-08-30 14:27:01 +0300

    new adjustments

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,76 +1,45 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = { ensure_installed = { "go", "gomod", "gowork", "gosum" } },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        opts = {
-            servers = {
-            gopls = {
-                settings = {
-                gopls = {
-                    gofumpt = true,
-                    codelenses = {
-                    gc_details = false,
-                    generate = true,
-                    regenerate_cgo = true,
-                    run_govulncheck = true,
-                    test = true,
-                    tidy = true,
-                    upgrade_dependency = true,
-                    vendor = true,
-                    },
-                    hints = {
-                    assignVariableTypes = true,
-                    compositeLiteralFields = true,
-                    compositeLiteralTypes = true,
-                    constantValues = true,
-                    functionTypeParameters = true,
-                    parameterNames = true,
-                    rangeVariableTypes = true,
-                    },
-                    analyses = {
-                    fieldalignment = true,
-                    nilness = true,
-                    unusedparams = true,
-                    unusedwrite = true,
-                    useany = true,
-                    },
-                    usePlaceholders = true,
-                    completeUnimported = true,
-                    staticcheck = true,
-                    directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-                    semanticTokens = true,
-                },
-                },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "go", "gomod", "gowork", "gosum", "html" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        html = {
+          settings = {
+            html = {
+              format = {
+                enable = true,
+              },
+              validate = {
+                enable = true,
+              },
             },
-            },
-            setup = {
-            gopls = function(_, opts)
-                -- workaround for gopls not supporting semanticTokensProvider
-                -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
-                LazyVim.lsp.on_attach(function(client, _)
-                if not client.server_capabilities.semanticTokensProvider then
-                    local semantic = client.config.capabilities.textDocument.semanticTokens
-                    client.server_capabilities.semanticTokensProvider = {
-                    full = true,
-                    legend = {
-                        tokenTypes = semantic.tokenTypes,
-                        tokenModifiers = semantic.tokenModifiers,
-                    },
-                    range = true,
-                    }
-                end
-                end, "gopls")
-                -- end workaround
-            end,
-            },
+          },
         },
-
+      },
     },
-    {
-        "williamboman/mason.nvim",
-        opts = { ensure_installed = { "goimports", "gofumpt" } },
-    }
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "goimports", "gofumpt", "html-lsp" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      if LazyVim.pick.want() ~= "telescope" then
+        return
+      end
+      local Keys = require("lazyvim.plugins.lsp.keymaps").get()
+            -- stylua: ignore
+            vim.list_extend(Keys, {
+            { "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, desc = "Goto Definition", has = "definition" },
+            { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References", nowait = true },
+            { "gI", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end, desc = "Goto Implementation" },
+            { "gy", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Goto T[y]pe Definition" },
+            })
+    end,
+  },
 }
