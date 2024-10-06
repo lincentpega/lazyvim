2bf0df2a664dd0d06372797ddb11e80dfe96a73b
lincentpega <imkrasnyukov@gmail.com>
2024-10-06 21:27:01 +0500

    add some changes

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,4 +1,1 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
 vim.opt.langmap = 'ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz'
-
